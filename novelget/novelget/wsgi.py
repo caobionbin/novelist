3792094d1625ae13bf5c5070e19a0575127170e1
zhangdesheng <jzhangdesheng@gmail.com>
2016-06-17 03:09:17 +0000

    wsgi

--- novelget/novelget/wsgi.py
+++ novelget/novelget/wsgi.py
@@ -8,9 +8,16 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+path = '/home/zhangdesheng/django-web/novelist/novelget'
+if path not in sys.path:
+    sys.path.append(path)
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "novelget.settings")
 
-application = get_wsgi_application()
+#application = get_wsgi_application()
+application = DjangoWhiteNoise(get_wsgi_application())
