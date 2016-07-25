6669175ff9f2cd610e9619dc09a14af9b7deebdd
zhangdesheng <jzhangdesheng@gmail.com>
2016-07-25 13:46:49 +0800

    抓取章节

--- novelget/novel/fetch_all_novels.py
+++ novelget/novel/fetch_all_novels.py
@@ -6,8 +6,9 @@ from time import gmtime, strftime
 import requests
 from bs4 import BeautifulSoup
 import re
+import os.path, os
 
-from fetch_novel import Get_Novel_Info, Save_Content
+from fetch_novel import Get_Novel_Info, Save_Content, get_chapter_content
 
 header = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.11; rv:46.0) Gecko/20100101 Firefox/46.0'}
 
@@ -135,5 +136,67 @@ def get_noveldata(id='bqg5200', content=None, url=None):
     return noveldata
 
 
+def get_novel_chapters():
+    conn = pymysql.connect(host='119.29.201.221', port=3306, user='novelist', db='novel', password='novelist#')
+    cur = conn.cursor()
+    sql = "select book_id, book_website, book_index_url from novel_book "
+    cur.execute(sql)
+    cur2 = conn.cursor()
+    for row in cur:
+        book_id, book_website, book_index_url = row[0], row[1], row[2]
+        cur2.execute("select count(1) from novel_bookchapter where book_id = %s " % book_id)
+        chapter_count = 0
+        for r in cur2:
+            chapter_count = int(r[0])
+        if chapter_count:
+            continue
+        noveldata = {}
+        noveldata['id'] = book_website
+        noveldata['content_link'] = book_index_url
+        try:
+            chapters = Save_Content(noveldata)
+            if chapters == -1:
+                continue
+            for chapter_num, chapter in enumerate(chapters):
+                # print(chapter)
+                chapter_title, chapter_url = chapter
+                bookchapter = None
+                sql = "insert into novel_bookchapter (book_id, chapter_name, chapter_url, chapter_num, chapter_content_path) " \
+                      "VALUES (%s, '%s', '%s', %s, '')" % (book_id, chapter_title, chapter_url, chapter_num)
+                cur2.execute(sql)
+        except Exception as e:
+            print(e)
+            continue
+    conn.commit()
+    cur.close()
+    cur2.close()
+    conn.close()
+
+
+def save_all_chapters():
+    save_dir = '/tmp/novelist/book'
+    if not os.path.exists(save_dir):
+        os.mkdir(save_dir)
+    sql = "select c.book_id, c.chapter_num, c.chapter_url, c.chapter_name, b.book_website from novel_bookchapter c, novel_book b where " \
+          "b.book_id = c.book_id order by c.book_id, c.chapter_num "
+    conn = pymysql.connect(host='119.29.201.221', port=3306, user='novelist', db='novel', password='novelist#')
+    cur = conn.cursor()
+    cur.execute(sql)
+    for row in cur:
+        book_id, chapter_num, chapter_url, chapter_name, book_website = row
+        print(book_id, chapter_num, chapter_url, book_website)
+        book_dir = os.path.join(save_dir, str(book_id))
+        if not os.path.exists(book_dir):
+            os.mkdir(book_dir)
+            with open(os.path.join(book_dir, str(chapter_num)), "w") as fw:
+                content = get_chapter_content(chapter_url, book_website)
+                if content == '未找到章节内容,刷新重试':
+                    print('can not get the content')
+                    continue
+                fw.write(chapter_name+'\n'+content)
+                time.sleep(0.2)
+    cur.close()
+    conn.close()
+
 if __name__ == '__main__':
-    fetch_toplist_books()
+    save_all_chapters()
