173cabb4aa7d02f44a569a729f798e51b6a05ece
zhangdesheng <jzhangdesheng@gmail.com>
2016-08-01 14:33:14 +0800

    Update settings.py

--- novelget/novelget/settings.py
+++ novelget/novelget/settings.py
@@ -83,8 +83,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'novel',
         'USER': 'novelist',
-        'PASSWORD': 'novelist#',
-        'HOST': '119.29.201.221',
+        'PASSWORD': '',
+        'HOST': '',
         'PORT': '3306',
         'OPTIONS': {
             'read_default_file': '/tmp/novelist/my.cnf',
